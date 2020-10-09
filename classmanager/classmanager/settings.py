39f024212348b283670ea11e7d67771173bfb409
Vicktron <tamsvictorisaac@gmail.com>
2020-10-09 04:36:59 +0100

    Ready for Deployment

--- classmanager/classmanager/settings.py
+++ classmanager/classmanager/settings.py
@@ -93,12 +93,44 @@ WSGI_APPLICATION = 'classmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'classmanager/../db.sqlite3'),
+
+# This is the Database before i Used Cloud SQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'classmanager/../db.sqlite3'),
+#     }
+# }
+
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/equest-website:us-central1:equest',
+            'USER': 'equest',
+            'PASSWORD': 'bGlo1G12yO9zj5zz',
+            'NAME': 'equest',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'equest',
+            'USER': 'equest',
+            'PASSWORD': 'bGlo1G12yO9zj5zz',
+        }
+    }
+# [END db_setup]
 
 
 # Password validation
