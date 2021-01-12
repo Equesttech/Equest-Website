34e45903107e65fdf9580687f46aed28ef0bba6a
Vicktron <tamsvictorisaac@gmail.com>
2021-01-12 17:35:07 +0100

    Changed the static directory

--- classmanager/wsgi.py
+++ classmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'classmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Equest Website.classmanager.settings')
 
 application = get_wsgi_application()
